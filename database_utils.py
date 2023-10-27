b83f511b4619534bf64e5ba0479c7d34ed644e81
Kyle Matthews <linescrawler@gmail.com>
2023-10-27 21:35:36 +0100

    Added method that reads the database credentials and returns an sqlalchemy databse engine.

--- database_utils.py
+++ database_utils.py
@@ -1,2 +1,34 @@
+import yaml
+import sqlalchemy
+
 class DatabaseConnector:
+    def read_db_creds(self):
+        """
+        The function reads database credentials from a YAML file.
+        """
+        with open('db_creds.yaml', 'r') as stream:
+            data_loaded = yaml.safe_load(stream)
+
+        return data_loaded
     
+    #Now create a method init_db_engine which will read the credentials 
+    # from the return of read_db_creds and initialise and return an sqlalchemy database engine.
+
+    def init_db_engine(self):
+        db_creds = self.read_db_creds()
+        db_url = sqlalchemy.engine.url.URL(
+                     
+            drivername=db_creds['drivername'],
+            username=db_creds['username'],
+            password=db_creds['password'],
+            host=db_creds['host'],
+            port=db_creds['port'],
+            database=db_creds['database']
+
+                )
+        
+        engine = sqlalchemy.create_engine(db_url)
+        return engine
+
+
+        return 
