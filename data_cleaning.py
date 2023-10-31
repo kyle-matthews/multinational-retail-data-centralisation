f6108d934ba7c78d38833d11eae3ce685f1a4509
Kyle Matthews <linescrawler@gmail.com>
2023-10-31 20:51:14 +0000

    Adds script to remove invalid values in user_info

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,1 +1,22 @@
+import pandas as pd
+import numpy as np
 class DataCleaning:
+    def clean_user_data(self, user_data):
+        """
+        The function `clean_user_data` cleans a DataFrame by dropping rows with missing values in the 'date'
+        and 'age' columns.
+        
+        :param user_data: The `user_data` parameter is a pandas DataFrame that contains user data. It is
+        assumed that the DataFrame has columns named 'date' and 'age'. The 'date' column is expected to
+        contain date values, and the 'age' column is expected to contain numeric age values
+        :return: the cleaned user data, which is a pandas DataFrame.
+        """
+        user_data = user_data.dropna()
+
+        user_data['date'] = pd.to_datetime(user_data['date'], errors='coerce')
+        user_data = user_data.dropna(subset=['date'])
+
+        user_data['age'] = pd.to_numeric(user_data['age'], errors='coerce')
+        user_data = user_data.dropna(subset=['age'])
+
+        return user_data
--- data_extraction.py
+++ data_extraction.py
@@ -3,4 +3,5 @@ from database_utils import DatabaseConnector
 
 class DataExtractor:
     def read_rds_table(self, DatabaseConnector, table_name):
-        list_db_tables(self)
+        DatabaseConnector.list_db_tables()
+        
