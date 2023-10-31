47cee631abf1392504ef1f8badb519252626c0bd
Kyle Matthews <linescrawler@gmail.com>
2023-10-31 21:43:55 +0000

    Updates to scripts

--- data_extraction.py
+++ data_extraction.py
@@ -1,7 +1,11 @@
-import pandas
+import pandas as pd
+import numpy as np
 from database_utils import DatabaseConnector
 
+# The DataExtractor class has a method called upload_to_db that uploads a DataFrame to a database
+# table.
 class DataExtractor:
-    def read_rds_table(self, DatabaseConnector, table_name):
-        DatabaseConnector.list_db_tables()
+        def upload_to_db(self, df, table_name):
+        DatabaseConnector.engine = self.init_db_engine()
+        df.to_sql(table_name, engine, if_exists='replace', index=False)
         
