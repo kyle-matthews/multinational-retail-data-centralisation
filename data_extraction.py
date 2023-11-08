88084deba863b88f24f8a22acf65985300c79fbd
Kyle Matthews <linescrawler@gmail.com>
2023-11-08 20:48:34 +0000

    Latest attempt

--- data_extraction.py
+++ data_extraction.py
@@ -2,10 +2,16 @@ import pandas as pd
 import numpy as np
 from database_utils import DatabaseConnector
 
-# The DataExtractor class has a method called upload_to_db that uploads a DataFrame to a database
-# table.
+
 class DataExtractor:
+        def read_rds_table(self, db_connector, table_name):
+            engine = db_connector.init_db_engine()
+            query = f"SELECT * FROM {table_name};"
+            df = pd.read_sql(query, engine)
+            return df
+
+
         def upload_to_db(self, df, table_name):
-        DatabaseConnector.engine = self.init_db_engine()
-        df.to_sql(table_name, engine, if_exists='replace', index=False)
-        
+            engine = self.init_db_engine()
+            df.to_sql(table_name, engine, if_exists='replace', index=False)
+
