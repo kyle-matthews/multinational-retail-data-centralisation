a7e1cc6c6b2d6ae58e8803ef107cb4f55353427f
Kyle Matthews <linescrawler@gmail.com>
2023-11-19 14:43:09 +0000

    Refactors existing code.

--- data_extraction.py
+++ data_extraction.py
@@ -10,16 +10,5 @@ class DataExtractor:
             df = pd.read_sql_table(table_name, engine)
             return df
 
-"""# Create an instance of the DatabaseConnector class
-db_connector = DatabaseConnector()
 
-# Create an instance of the DataExtractor class
-data_extractor = DataExtractor()
-
-# Read data from the database and store it in a DataFrame
-table_name = 'car_sales'
-dim_users = data_extractor.read_rds_table(db_connector, table_name)
-
-# Upload data to the database
-data_extractor.upload_to_db(dim_users, table_name, db_connector)"""
 
