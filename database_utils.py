1590ace9228f01e0e384bc71646a34d950db022e
Kyle Matthews <linescrawler@gmail.com>
2023-11-20 22:07:38 +0000

    Adds latest attempt at fixing upload_to_db error

--- database_utils.py
+++ database_utils.py
@@ -1,48 +1,41 @@
 import yaml
-import sqlalchemy
+from sqlalchemy import create_engine
 import psycopg2
 
 class DatabaseConnector:
-    def read_db_creds(self):
+    def read_db_creds(self, creds):
         """
         The function reads database credentials from a YAML file.
         """
-        with open('db_creds.yaml', 'r') as file:
+        with open(creds, 'r') as file:
             db_credentials = yaml.safe_load(file)
 
         return db_credentials
     
-    #Now create a method init_db_engine which will read the credentials 
-    # from the return of read_db_creds and initialise and return an sqlalchemy database engine.
-
-    def read_pgadmin_creds(self):
-        with open('PgAdmin.yaml', 'r') as file:
-            pg_admin_creds = yaml.safe_load(file)
-        return pg_admin_creds
 
     def init_db_engine(self):
         """
-        The function `init_db_engine` initialises a database engine using the credentials read from a
-        file.
-        :return: a SQLAlchemy engine object.
+        The `init_db_engine` function initializes a database engine using the credentials read from a file
+        and returns the engine.
+        :return: The `self.engine` object is being returned.
         """
-        db_creds = self.read_db_creds()
-        """db_url = sqlalchemy.engine.url.URL(
-            
-            username=db_creds['RDS_USER'],
-            password=db_creds['RDS_PASSWORD'],
-            host=db_creds['RDS_HOST'],
-            port=db_creds['RDS_PORT'],
-            database=db_creds['RDS_DATABASE']
 
-                )"""
-        db_url = f"postgresql+psycopg2://{db_creds['RDS_USER']}:{db_creds['RDS_PASSWORD']}@{db_creds['RDS_HOST']}:{db_creds['RDS_PORT']}/{db_creds['RDS_DATABASE']}"
+        creds = self.read_db_creds()
         
-        engine = sqlalchemy.create_engine(db_url)
-        return engine
+        DATABASE_TYPE = 'postgresql'
+        DBAPI = 'psycopg2'
+        HOST = creds['HOST']
+        PASSWORD = creds['PASSWORD']
+        USER = creds['USER']
+        DATABASE = creds['DATABASE']
+        PORT = creds['PORT']
+        self.engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
+        print('Database connected')
+        return self.engine
+
     
 
-    def list_db_tables(self):
+    """def list_db_tables(self):
 # The code block you provided is defining a method called `list_db_tables` in the `DatabaseConnector`
 # class. This method is responsible for connecting to the database, inspecting the tables in the
 # database, and returning a list of table names.
@@ -51,22 +44,12 @@ class DatabaseConnector:
         tables = table_inspector.get_table_names()
 
         print(tables)
-        return tables
-
+        return tables"""
+    
 
     def upload_to_db(self, df, table_name):
 
-        pg_admin = self.read_pgadmin_creds()
-
-        # Connect to the database
-        conn = psycopg2.connect(host=pg_admin['HOST'],
-                                dbname=pg_admin['DB_NAME'],
-                                user=pg_admin['USER'],
-                                password=pg_admin['PASSWORD'])
-        cur = conn.cursor()
-
         engine = self.init_db_engine()
 
-        df.to_sql(name=table_name, con=engine, if_exists='replace')
+        df.to_sql(name=table_name, con=engine, if_exists='replace', index=False)
         print('Data successfully uploaded.')
-        
--- main.py
+++ main.py
@@ -5,16 +5,15 @@ connector = DatabaseConnector()
 extractor = DataExtractor()
 cleaner = DataCleaning()
 
-connector.list_db_tables()
-
-table_name = 'legacy_users'
-df = extractor.read_rds_table(connector, table_name)
-#print(legacy_users)
+creds = 'db_creds.yaml'
+df = extractor.read_rds_table(connector, 'legacy_users')
 
 clean_df = cleaner.clean_user_data(df)
 
-
 connector.upload_to_db(clean_df, 'dim_users')
+
+
+
 
 #payment_data = extractor.retrieve_pdf_data('https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf')
 
