ada76605c75266559113ad0992e83d0b95e89dce
Kyle Matthews <linescrawler@gmail.com>
2023-11-22 21:30:11 +0000

    Adds tools to extract data from a PDF and present it in a dataframe.

--- main.py
+++ main.py
@@ -8,11 +8,10 @@ cleaner = DataCleaning()
 creds = 'db_creds.yaml'
 df = extractor.read_rds_table(connector, 'legacy_users')
 
-clean_df = cleaner.clean_user_data(df)
+#clean_df = cleaner.clean_user_data(df)
 
-connector.upload_to_db(clean_df, 'dim_users')
+#connector.upload_to_db(clean_df, 'dim_users')
 
+payment_data = extractor.retrieve_pdf_data('https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf')
 
-#payment_data = extractor.retrieve_pdf_data('https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf')
-
-#print(payment_data)
+print(payment_data)
