972606c5f057d207c97095ebee1c9c6339bb91ad
Kyle Matthews <linescrawler@gmail.com>
2023-12-17 06:23:32 +0000

    Tidies up some SQL queries and begins to solve an error with casting the weight column to double precision. NaN values to be cleaned.

--- change_data_types.sql
+++ change_data_types.sql
@@ -1,69 +1,66 @@
 --Changes data types to correct formatting in 'orders_table' table.
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+/*CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 ALTER TABLE orders_table
-    ALTER COLUMN date_uuid SET DATA TYPE UUID USING (uuid_generate_v4());
-
-ALTER TABLE orders_table 
-    ALTER COLUMN user_uuid SET DATA TYPE UUID USING (uuid_generate_v4());
-
-ALTER TABLE orders_table
-    ALTER COLUMN card_number TYPE VARCHAR(19);
-
-ALTER TABLE orders_table
-    ALTER COLUMN store_code TYPE VARCHAR(12);
-
-ALTER TABLE orders_table
-    ALTER COLUMN product_code TYPE VARCHAR(11);
-
-ALTER TABLE orders_table
+    ALTER COLUMN date_uuid SET DATA TYPE UUID USING (uuid_generate_v4()),
+    ALTER COLUMN user_uuid SET DATA TYPE UUID USING (uuid_generate_v4()),
+    ALTER COLUMN card_number TYPE VARCHAR(19),
+    ALTER COLUMN store_code TYPE VARCHAR(12),
+    ALTER COLUMN product_code TYPE VARCHAR(11),
     ALTER COLUMN product_quantity TYPE SMALLINT;
 
 
 --Changes data types to correct formatting in 'dim_users' table.
 
 ALTER TABLE dim_users
-    ALTER COLUMN first_name TYPE VARCHAR(255);
-
-ALTER TABLE dim_users
-    ALTER COLUMN last_name TYPE VARCHAR(255);
-
-ALTER TABLE dim_users
-    ALTER COLUMN date_of_birth TYPE DATE;
-
-ALTER TABLE dim_users
-    ALTER COLUMN country_code TYPE VARCHAR(3);
-
-ALTER TABLE dim_users
-    ALTER COLUMN user_uuid TYPE UUID USING (uuid_generate_v4());
-
-ALTER TABLE dim_users
+    ALTER COLUMN first_name TYPE VARCHAR(255),
+    ALTER COLUMN last_name TYPE VARCHAR(255),
+    ALTER COLUMN date_of_birth TYPE DATE,
+    ALTER COLUMN country_code TYPE VARCHAR(3),
+    ALTER COLUMN user_uuid TYPE UUID USING (uuid_generate_v4()),
     ALTER COLUMN join_date TYPE DATE;
 
 --This should update the data types in the dim_store_details table. (Should.)
 ALTER TABLE dim_store_details
-    ALTER COLUMN longitude TYPE FLOAT; 
+    ALTER COLUMN longitude TYPE FLOAT,
+    ALTER COLUMN locality TYPE VARCHAR(255),
+    ALTER COLUMN store_code TYPE VARCHAR(12),
+    ALTER COLUMN staff_numbers TYPE integer USING (trim(staff_numbers)::integer),
+    ALTER COLUMN opening_date TYPE DATE,
+    ALTER COLUMN store_type TYPE VARCHAR(255),
+    ALTER COLUMN latitude TYPE FLOAT,
+    ALTER COLUMN country_code TYPE VARCHAR(2),
+    ALTER COLUMN continent TYPE VARCHAR(255);*/
 
-ALTER TABLE dim_store_details
-    ALTER COLUMN locality TYPE VARCHAR(255);
+--Alterations to the dim_products table. 
+UPDATE dim_products
+    SET product_price = REPLACE(
+        product_price,
+        '£',
+        ''
+        )::FLOAT;
+ALTER TABLE dim_products
+    ADD COLUMN weight_class VARCHAR(50);
 
-ALTER TABLE dim_store_details
-    ALTER COLUMN store_code TYPE VARCHAR(12);
+ALTER TABLE dim_products
+    RENAME COLUMN removed TO still_available;
 
-ALTER TABLE dim_store_details
-    ALTER COLUMN staff_numbers TYPE integer USING (trim(staff_numbers)::integer);
 
-ALTER TABLE dim_store_details
-    ALTER COLUMN opening_date TYPE DATE;
+ALTER TABLE dim_products
+    ALTER COLUMN weight TYPE FLOAT,
+    ALTER COLUMN EAN TYPE VARCHAR(255),
+    ALTER COLUMN product_code TYPE VARCHAR(255),
+    ALTER COLUMN date_added TYPE DATE,
+    ALTER COLUMN uuid TYPE UUID,
+    ALTER COLUMN still_available TYPE BOOL,
+    ALTER COLUMN weight_class TYPE VARCHAR(255);
 
-ALTER TABLE dim_store_details
-    ALTER COLUMN store_type TYPE VARCHAR(255);
+UPDATE dim_products
+    SET weight_class = 
+        CASE
+            WHEN weight < 10 THEN 'Light'
+            WHEN weight >= 10 AND weight < 50 THEN 'Medium'
+            WHEN weight >= 50 THEN 'Heavy'
+        END;
 
-ALTER TABLE dim_store_details
-    ALTER COLUMN latitude TYPE FLOAT;
 
-ALTER TABLE dim_store_details
-    ALTER COLUMN country_code TYPE VARCHAR(2);
-
-ALTER TABLE dim_store_details
-    ALTER COLUMN continent TYPE VARCHAR(255);
