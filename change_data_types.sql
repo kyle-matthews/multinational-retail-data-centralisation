1c9f98c3aa711d2f63a69f06a22db94dd81eba12
Kyle Matthews <linescrawler@gmail.com>
2023-12-28 18:25:53 +0000

    reformats some code for enhanced human readability.

--- change_data_types.sql
+++ change_data_types.sql
@@ -1,110 +1,174 @@
 --Changes data types to correct formatting in 'orders_table' table.
 
 ALTER TABLE orders_table
-    ALTER COLUMN date_uuid TYPE UUID USING date_uuid::UUID;
+ALTER COLUMN date_uuid TYPE UUID USING date_uuid::UUID;
+
+
 ALTER TABLE orders_table
-    ALTER COLUMN user_uuid TYPE UUID USING user_uuid::UUID;
+ALTER COLUMN user_uuid TYPE UUID USING user_uuid::UUID;
+
+
 ALTER TABLE orders_table
-    ALTER COLUMN card_number TYPE VARCHAR(25);
+ALTER COLUMN card_number TYPE VARCHAR(25);
+
+
 ALTER TABLE orders_table
-    ALTER COLUMN store_code TYPE VARCHAR(12);
+ALTER COLUMN store_code TYPE VARCHAR(12);
+
+
 ALTER TABLE orders_table
-    ALTER COLUMN product_code TYPE VARCHAR(11);
+ALTER COLUMN product_code TYPE VARCHAR(11);
+
+
 ALTER TABLE orders_table
-    ALTER COLUMN product_quantity TYPE SMALLINT;
+ALTER COLUMN product_quantity TYPE SMALLINT;
 
 --Changes data types to correct formatting in 'dim_users' table.
 
 ALTER TABLE dim_users
-    ALTER COLUMN first_name TYPE VARCHAR(255);
+ALTER COLUMN first_name TYPE VARCHAR(255);
+
+
 ALTER TABLE dim_users
-    ALTER COLUMN last_name TYPE VARCHAR(255);
+ALTER COLUMN last_name TYPE VARCHAR(255);
+
+
 ALTER TABLE dim_users
-    ALTER COLUMN date_of_birth TYPE DATE;
+ALTER COLUMN date_of_birth TYPE DATE;
+
+
 ALTER TABLE dim_users
-    ALTER COLUMN country_code TYPE VARCHAR(3);
+ALTER COLUMN country_code TYPE VARCHAR(3);
+
+
 ALTER TABLE dim_users
-    ALTER COLUMN user_uuid TYPE UUID USING user_uuid::UUID;
+ALTER COLUMN user_uuid TYPE UUID USING user_uuid::UUID;
+
+
 ALTER TABLE dim_users
-    ALTER COLUMN join_date TYPE DATE;
+ALTER COLUMN join_date TYPE DATE;
 
 --This should update the data types in the dim_store_details table. (Should.)
-ALTER TABLE dim_store_details
-    ALTER COLUMN longitude TYPE FLOAT;
-ALTER TABLE dim_store_details
-    ALTER COLUMN locality TYPE VARCHAR(255);
-ALTER TABLE dim_store_details
-    ALTER COLUMN store_code TYPE VARCHAR(12);
-ALTER TABLE dim_store_details
-    ALTER COLUMN staff_numbers TYPE integer USING (trim(staff_numbers)::integer);
-ALTER TABLE dim_store_details
-    ALTER COLUMN opening_date TYPE DATE;
-ALTER TABLE dim_store_details
-    ALTER COLUMN store_type TYPE VARCHAR(255);
-ALTER TABLE dim_store_details
-    ALTER COLUMN latitude TYPE FLOAT;
-ALTER TABLE dim_store_details
-    ALTER COLUMN country_code TYPE VARCHAR(2);
-ALTER TABLE dim_store_details
-    ALTER COLUMN continent TYPE VARCHAR(255);
 
---Alterations to the dim_products table. 
-UPDATE dim_products
-    SET product_price = REPLACE(product_price,'£','')::FLOAT;
-ALTER TABLE dim_products
-    ADD COLUMN weight_class VARCHAR(50);
-
-ALTER TABLE dim_products
-    RENAME COLUMN removed TO still_available;
+ALTER TABLE dim_store_details
+ALTER COLUMN longitude TYPE FLOAT;
 
 
-ALTER TABLE dim_products
-    ALTER COLUMN weight TYPE double precision USING (trim(weight)::double precision);
-ALTER TABLE dim_products
-    ALTER COLUMN EAN TYPE VARCHAR(255);
-ALTER TABLE dim_products
-    ALTER COLUMN product_code TYPE VARCHAR(255);
-ALTER TABLE dim_products
-    ALTER COLUMN date_added TYPE DATE;
-ALTER TABLE dim_products
-    ALTER COLUMN uuid TYPE UUID USING uuid::UUID;
-ALTER TABLE dim_products
-    ALTER COLUMN weight_class TYPE VARCHAR(255);
+ALTER TABLE dim_store_details
+ALTER COLUMN locality TYPE VARCHAR(255);
+
+
+ALTER TABLE dim_store_details
+ALTER COLUMN store_code TYPE VARCHAR(12);
+
+
+ALTER TABLE dim_store_details
+ALTER COLUMN staff_numbers TYPE integer USING (trim(staff_numbers)::integer);
+
+
+ALTER TABLE dim_store_details
+ALTER COLUMN opening_date TYPE DATE;
+
+
+ALTER TABLE dim_store_details
+ALTER COLUMN store_type TYPE VARCHAR(255);
+
+
+ALTER TABLE dim_store_details
+ALTER COLUMN latitude TYPE FLOAT;
+
+
+ALTER TABLE dim_store_details
+ALTER COLUMN country_code TYPE VARCHAR(2);
+
+
+ALTER TABLE dim_store_details
+ALTER COLUMN continent TYPE VARCHAR(255);
+
+--Alterations to the dim_products table.
 
 UPDATE dim_products
-    SET still_available = REPLACE(still_available,'Still_avaliable','True');
-UPDATE dim_products
-    SET still_available = REPLACE(still_available,'Removed','False');
+SET product_price = REPLACE(product_price,'£','')::FLOAT;
+
+
+ALTER TABLE dim_products ADD COLUMN weight_class VARCHAR(50);
+
+
+ALTER TABLE dim_products RENAME COLUMN removed TO still_available;
+
 
 ALTER TABLE dim_products
-    ALTER COLUMN still_available TYPE BOOLEAN USING (still_available::BOOLEAN);
+ALTER COLUMN weight TYPE double precision USING (trim(weight)::double precision);
+
+
+ALTER TABLE dim_products
+ALTER COLUMN EAN TYPE VARCHAR(255);
+
+
+ALTER TABLE dim_products
+ALTER COLUMN product_code TYPE VARCHAR(255);
+
+
+ALTER TABLE dim_products
+ALTER COLUMN date_added TYPE DATE;
+
+
+ALTER TABLE dim_products
+ALTER COLUMN uuid TYPE UUID USING uuid::UUID;
+
+
+ALTER TABLE dim_products
+ALTER COLUMN weight_class TYPE VARCHAR(255);
 
 
 UPDATE dim_products
-    SET weight_class = 
-        CASE
-            WHEN weight < 10 THEN 'Light'
-            WHEN weight >= 10 AND weight < 50 THEN 'Medium'
-            WHEN weight >= 50 THEN 'Heavy'
-        END;
+SET still_available = REPLACE(still_available,'Still_avaliable','True');
 
 
+UPDATE dim_products
+SET still_available = REPLACE(still_available,'Removed','False');
+
+
+ALTER TABLE dim_products
+ALTER COLUMN still_available TYPE BOOLEAN USING (still_available::BOOLEAN);
+
+
+UPDATE dim_products
+SET weight_class = CASE
+                       WHEN weight < 10 THEN 'Light'
+                       WHEN weight >= 10
+                            AND weight < 50 THEN 'Medium'
+                       WHEN weight >= 50 THEN 'Heavy'
+                   END;
 
 
 ALTER TABLE dim_date_times
-    ALTER COLUMN month TYPE VARCHAR(100);
+ALTER COLUMN month TYPE VARCHAR(100);
+
+
 ALTER TABLE dim_date_times
-    ALTER COLUMN year TYPE VARCHAR(100);
+ALTER COLUMN year TYPE VARCHAR(100);
+
+
 ALTER TABLE dim_date_times
-    ALTER COLUMN day TYPE VARCHAR (100);
+ALTER COLUMN day TYPE VARCHAR (100);
+
+
 ALTER TABLE dim_date_times
-    ALTER COLUMN time_period TYPE VARCHAR(100);
+ALTER COLUMN time_period TYPE VARCHAR(100);
+
+
 ALTER TABLE dim_date_times
-    ALTER COLUMN date_uuid TYPE UUID USING date_uuid::UUID;
+ALTER COLUMN date_uuid TYPE UUID USING date_uuid::UUID;
+
 
 ALTER TABLE dim_card_details
-    ALTER COLUMN card_number TYPE VARCHAR(25);
+ALTER COLUMN card_number TYPE VARCHAR(25);
+
+
 ALTER TABLE dim_card_details
-    ALTER COLUMN expiry_date TYPE VARCHAR(5);
+ALTER COLUMN expiry_date TYPE VARCHAR(5);
+
+
 ALTER TABLE dim_card_details
-    ALTER COLUMN date_payment_confirmed TYPE DATE USING date_payment_confirmed::date;
+ALTER COLUMN date_payment_confirmed TYPE DATE USING date_payment_confirmed::date;
--- data_cleaning.py
+++ data_cleaning.py
@@ -5,46 +5,55 @@ import numpy as np
 
 
 class DataCleaning:
-
     def clean_user_data(self, df):
         """
         The function `clean_user_data` takes a DataFrame as input, removes nonsense values, cleans the phone
         number and join date columns, converts the date of birth and join date columns to datetime format,
         drops rows with missing values, and replaces 'GGB' with 'GB' in the country code column.
-        
+
         :param df: The parameter `df` is a pandas DataFrame that contains user data
         """
-        #Aiming for 15284 rows
+        # Aiming for 15284 rows
         # Remove nonsense values
-        print('OG df' + str(len(df)))
+        print("OG df" + str(len(df)))
         df = self.remove_nonsense(df)
-        print('removed nonsense ' + str(len(df)))
+        print("removed nonsense " + str(len(df)))
         # Reset index
         #
         df = df.reset_index(drop=True)
-        df = df[df.first_name != 'NULL']
+        df = df[df.first_name != "NULL"]
         # Clean phone number column
-        df.loc[:, 'phone_number'] = df.loc[:,'phone_number'].str.replace(r'^(?:\(\+\d+\))|\D', '', regex=True)
-        print('replacing non digit in phone number ' + str(len(df)))
-        #df['join_date'] = df['join_date'].str.replace(r'^(?:\(\+\d+\))|\D', '', regex=True)
-        
-        # Convert date_of_birth and join_date to datetime format
-        df.loc[:, 'date_of_birth'] = df.loc[:, 'date_of_birth'].apply(pd.to_datetime, errors='ignore')
-        print('DOB datetime ' + str(len(df)))
-        df.loc[:, 'join_date'] = df.loc[:, 'join_date'].apply(pd.to_datetime, errors='ignore')
-        print('Join date Datetime ' + str(len(df)))
-        # Drop rows with missing values in specified columns - Removed due to inconsistencies between foreign key user_uuid
-        #df.dropna(subset=['date_of_birth', 'country_code'], inplace=True)
-        
-        # Replace 'GGB' with 'GB' in country_code column
-        df.loc[:, 'country_code'] = df.loc[:, 'country_code'].astype(str).apply(lambda x: x.replace('GGB', 'GB'))
-        print('Edit GGB to GB ' + str(len(df)))
+        df.loc[:, "phone_number"] = df.loc[:, "phone_number"].str.replace(
+            r"^(?:\(\+\d+\))|\D", "", regex=True
+        )
+        print("replacing non digit in phone number " + str(len(df)))
+        # df['join_date'] = df['join_date'].str.replace(r'^(?:\(\+\d+\))|\D', '', regex=True)
 
-        df = df.dropna(how='all', subset=['user_uuid'])
-        print('dropna ' + str(len(df)))
+        # Convert date_of_birth and join_date to datetime format
+        df.loc[:, "date_of_birth"] = df.loc[:, "date_of_birth"].apply(
+            pd.to_datetime, errors="ignore"
+        )
+        print("DOB datetime " + str(len(df)))
+        df.loc[:, "join_date"] = df.loc[:, "join_date"].apply(
+            pd.to_datetime, errors="ignore"
+        )
+        print("Join date Datetime " + str(len(df)))
+        # Drop rows with missing values in specified columns - Removed due to inconsistencies between foreign key user_uuid
+        # df.dropna(subset=['date_of_birth', 'country_code'], inplace=True)
+
+        # Replace 'GGB' with 'GB' in country_code column
+        df.loc[:, "country_code"] = (
+            df.loc[:, "country_code"]
+            .astype(str)
+            .apply(lambda x: x.replace("GGB", "GB"))
+        )
+        print("Edit GGB to GB " + str(len(df)))
+
+        df = df.dropna(how="all", subset=["user_uuid"])
+        print("dropna " + str(len(df)))
 
         return df
-  
+
     def clean_card_data(self, df):
         """
         The function `clean_card_data` removes rows with missing values in the 'card_number' and
@@ -52,21 +61,32 @@ class DataCleaning:
         'expiration_date' column to datetime type.
         :param df: The parameter `df` is a pandas DataFrame that contains card data
         """
-        card_list = ['Diners Club / Carte Blanche', 'American Express', 'JCB 16 digit', 'JCB 15 digit', 
-        'Maestro', 'Mastercard', 'Discover', 'VISA 19 digit',
-        'VISA 16 digit', 'VISA 13 digit']
-        df = df.dropna(how='all')
+        card_list = [
+            "Diners Club / Carte Blanche",
+            "American Express",
+            "JCB 16 digit",
+            "JCB 15 digit",
+            "Maestro",
+            "Mastercard",
+            "Discover",
+            "VISA 19 digit",
+            "VISA 16 digit",
+            "VISA 13 digit",
+        ]
+        df = df.dropna(how="all")
         print(df.head(5))
-        #df = self.remove_nonsense(df)
-        df = df[df['card_provider'].isin(card_list)]
+        # df = self.remove_nonsense(df)
+        df = df[df["card_provider"].isin(card_list)]
         print(df.head(5))
-        df.loc[:, 'card_number'] = df.loc[:, 'card_number'].astype(str).apply(lambda x: x.replace('?', ''))
+        df.loc[:, "card_number"] = (
+            df.loc[:, "card_number"].astype(str).apply(lambda x: x.replace("?", ""))
+        )
         print(df.head(5))
         df = df.reset_index(drop=True)
-        #print(df.head(5))
-        df['card_number'] = df['card_number'].apply(pd.to_numeric, errors='coerce')
+        # print(df.head(5))
+        df["card_number"] = df["card_number"].apply(pd.to_numeric, errors="coerce")
         print(df.head(5))
-        #df["date_payment_confirmed"] = df["date_payment_confirmed"].apply(lambda x: parse(x))
+        # df["date_payment_confirmed"] = df["date_payment_confirmed"].apply(lambda x: parse(x))
 
         return df
 
@@ -77,26 +97,30 @@ class DataCleaning:
         missing values in specific columns, dropping a column, removing rows with addresses shorter than 5
         characters, dropping duplicate rows based on specific columns, and resetting the index of the
         DataFrame.
-        
+
         :param df: The parameter `df` is a pandas DataFrame that contains store data
         :return: the cleaned and filtered dataframe.
         """
-        store_type = ['Web Portal', 'Local', 'Super Store', 'Mall Kiosk', 'Outlet']
-        df = df[df['store_type'].isin(store_type)]
-        df = df.dropna(how='all')
-        df.loc[:, 'longitude'] = pd.to_numeric(df.loc[:, 'longitude'], errors='coerce')
-        df.loc[:, 'latitude'] = pd.to_numeric(df.loc[:,'latitude'], errors='coerce')
-        print('After long/lat: ', df['store_code'].unique())
-        df.loc[:, 'staff_numbers'] = df.loc[:,'staff_numbers'].str.replace(r'^(?:\(\+\d+\))|\D', '', regex=True)
-        print('after staff numbers :', df['store_code'].unique())
-        df.loc[:, 'opening_date'] = pd.to_datetime(df.loc[:, 'opening_date'].astype(str), format='mixed', errors='coerce')
-        print('after date_time conversion :', df['store_code'].unique())
-        df = df.drop(['lat'], axis=1)
-        print('dropping lat :', df['store_code'].unique())
-        #df = df[df['address'].str.len() >= 3]
-        print('after address check :', df['store_code'].unique())
-        df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
-        
+        store_type = ["Web Portal", "Local", "Super Store", "Mall Kiosk", "Outlet"]
+        df = df[df["store_type"].isin(store_type)]
+        df = df.dropna(how="all")
+        df.loc[:, "longitude"] = pd.to_numeric(df.loc[:, "longitude"], errors="coerce")
+        df.loc[:, "latitude"] = pd.to_numeric(df.loc[:, "latitude"], errors="coerce")
+        print("After long/lat: ", df["store_code"].unique())
+        df.loc[:, "staff_numbers"] = df.loc[:, "staff_numbers"].str.replace(
+            r"^(?:\(\+\d+\))|\D", "", regex=True
+        )
+        print("after staff numbers :", df["store_code"].unique())
+        df.loc[:, "opening_date"] = pd.to_datetime(
+            df.loc[:, "opening_date"].astype(str), format="mixed", errors="coerce"
+        )
+        print("after date_time conversion :", df["store_code"].unique())
+        df = df.drop(["lat"], axis=1)
+        print("dropping lat :", df["store_code"].unique())
+        # df = df[df['address'].str.len() >= 3]
+        print("after address check :", df["store_code"].unique())
+        df = df.loc[:, ~df.columns.str.contains("^Unnamed")]
+
         df = df.reset_index(drop=True)
 
         return df
@@ -106,66 +130,79 @@ class DataCleaning:
         The `clean_product_weights` function takes a DataFrame as input, removes nonsense values, drops rows
         with missing weight values, cleans the weight column by removing units and special characters, and
         returns the cleaned DataFrame.
-        
+
         :param df: The `df` parameter is a pandas DataFrame that contains product data
         :return: the cleaned dataframe, `clean_df`.
         """
         clean_df = df.copy()
         clean_df.columns = clean_df.columns.str.lower()
         clean_df = self.remove_nonsense(clean_df)
-        clean_df = clean_df.dropna(subset=['weight'])
-        clean_df.loc[:, 'weight'] = clean_df.loc[:, 'weight'].str.strip('.')
-        clean_df.loc[:, 'date_added'] = pd.to_datetime(clean_df.loc[:, 'date_added'].astype(str), format='mixed', errors='coerce')
-        
-        clean_df.loc[:, 'weight'] = clean_df.loc[:,'weight'].apply(lambda x: x.replace('g', ''))
-        print('g replaced')
+        clean_df = clean_df.dropna(subset=["weight"])
+        clean_df.loc[:, "weight"] = clean_df.loc[:, "weight"].str.strip(".")
+        clean_df.loc[:, "date_added"] = pd.to_datetime(
+            clean_df.loc[:, "date_added"].astype(str), format="mixed", errors="coerce"
+        )
 
-        clean_df.loc[:, 'weight'] = clean_df.loc[:,'weight'].apply(lambda x: x.replace('ml', ''))
-        print('ml replaced')
-        
-        #This method of cleaning the oz's might break something
-        clean_df.loc[:, 'weight'] = clean_df.loc[:,'weight'].apply(lambda x: x.replace('oz', ''))
-        print('ml replaced')
+        clean_df.loc[:, "weight"] = clean_df.loc[:, "weight"].apply(
+            lambda x: x.replace("g", "")
+        )
+        print("g replaced")
 
-        clean_df.loc[:, 'weight'] = clean_df.loc[:,'weight'].str.replace(r'^[0-9]+\sx\s+[0-9]+$', '', regex=True)
-        clean_df.replace('', 0, inplace=True)
-        print('x gone')
+        clean_df.loc[:, "weight"] = clean_df.loc[:, "weight"].apply(
+            lambda x: x.replace("ml", "")
+        )
+        print("ml replaced")
 
-        clean_df.loc[:, 'weight'] = clean_df.loc[:, 'weight'].astype(str).apply(lambda x: float(x) /1000 if 'k' not in x else x)
+        # This method of cleaning the oz's might break something
+        clean_df.loc[:, "weight"] = clean_df.loc[:, "weight"].apply(
+            lambda x: x.replace("oz", "")
+        )
+        print("ml replaced")
 
-        clean_df.loc[:,'weight'] = clean_df.loc[:,'weight'].astype('str').apply(lambda x: x.replace('k', ''))
+        clean_df.loc[:, "weight"] = clean_df.loc[:, "weight"].str.replace(
+            r"^[0-9]+\sx\s+[0-9]+$", "", regex=True
+        )
+        clean_df.replace("", 0, inplace=True)
+        print("x gone")
 
+        clean_df.loc[:, "weight"] = (
+            clean_df.loc[:, "weight"]
+            .astype(str)
+            .apply(lambda x: float(x) / 1000 if "k" not in x else x)
+        )
+
+        clean_df.loc[:, "weight"] = (
+            clean_df.loc[:, "weight"].astype("str").apply(lambda x: x.replace("k", ""))
+        )
 
         return clean_df
 
-    def convert_weight(self,value):
+    def convert_weight(self, value):
         """
         The function `convert_weight` converts a weight value to a numeric format by removing any non-digit
         characters.
-        
+
         :param value: The `value` parameter is the weight value that needs to be converted
         :return: the numeric value of the input `value` after removing any non-digit characters. If the
         input `value` is `NaN` (not a number), it will return `NaN` itself.
         """
         if pd.notna(value):
-            return pd.to_numeric(re.sub(r'\D', '', str(value)))
+            return pd.to_numeric(re.sub(r"\D", "", str(value)))
         else:
             return value
 
     def remove_nonsense(self, df):
-
-
         """
         The function removes any values in the dataframe that consist of 8 to 10 uppercase letters or
         digits.
-        
+
         :param df: The parameter `df` is a pandas DataFrame object
         :return: the modified dataframe after removing the rows that match the specified regular expression
         pattern.
         """
-        df.replace(r'^[A-Z0-9]{8,10}$', None, inplace=True, regex=True)
+        df.replace(r"^[A-Z0-9]{8,10}$", None, inplace=True, regex=True)
         return df
-    
+
         """
         The function `drop_columns` is used to drop a specified column from a DataFrame in Python.
         
@@ -183,31 +220,31 @@ class DataCleaning:
         The function `clean_orders_data` takes a DataFrame as input, creates a copy of it, removes nonsense
         values, drops specific columns, drops rows with missing values in certain columns, and returns the
         cleaned DataFrame.
-        
+
         :param df: The parameter `df` is a pandas DataFrame that contains the orders data
         :return: the cleaned dataframe, `clean_df`.
         """
         # Create a copy of the DataFrame
         # Drop specific columns
-        df = df.drop(['level_0', '1', 'first_name', 'last_name'], axis = 1)
+        df = df.drop(["level_0", "1", "first_name", "last_name"], axis=1)
         # Remove nonsense values
         df = self.remove_nonsense(df)
 
         # Drop rows with missing values in specified columns
-        #clean_df = clean_df.dropna(subset=['first_name', 'last_name', '1'])
-        
+        # clean_df = clean_df.dropna(subset=['first_name', 'last_name', '1'])
+
         return df
 
     def clean_date_times(self, df):
         df = self.remove_nonsense(df)
-        print('nonsense removed')
-        df['month'] = pd.to_numeric(df['month'], errors='coerce')
-        print('month numericed')
-        df['year'] = pd.to_numeric(df['year'], errors='coerce')
-        print('year numericed')
-        df['day'] = pd.to_numeric(df['day'], errors='coerce')
-        print('day numericed')
+        print("nonsense removed")
+        df["month"] = pd.to_numeric(df["month"], errors="coerce")
+        print("month numericed")
+        df["year"] = pd.to_numeric(df["year"], errors="coerce")
+        print("year numericed")
+        df["day"] = pd.to_numeric(df["day"], errors="coerce")
+        print("day numericed")
         df = df.dropna()
-        print('df dropnad')
-        
+        print("df dropnad")
+
         return df
